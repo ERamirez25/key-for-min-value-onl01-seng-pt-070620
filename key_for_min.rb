98eebacdf9f2f997410efaeb3d8a34dc67bfcdc3
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-16 00:23:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,8 @@
 
-def key_for_min_value(name_hash)
+def key_for_min_value(hash)
   lowest_key == nil 
   lowest_value == nil 
-  name_hash.each do |key, value|
+   hash.each do |key, value|
     if lowest_value == nil || value < lowest_value 
       lowest_value = value 
       lowest_key = key 
